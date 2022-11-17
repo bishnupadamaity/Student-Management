567bf2f03905bf9ab1c2f47eed6c302c616621fb
Bishnupada Maity <bishnupdm.1@gmail.com>
2022-11-17 09:32:41 +0530

    initial commit

---
+++ src/main/java/com/MyManagement/MyManagementApplication.java
@@ -0,0 +1,13 @@
+package com.MyManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyManagementApplication.class, args);
+	}
+
+}

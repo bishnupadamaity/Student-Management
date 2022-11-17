14efc36c3fbb041dd242e2bf8bf944f012ebd377
Bishnupada Maity <bishnupdm.1@gmail.com>
2022-11-17 10:04:38 +0530

    add student entity

---
+++ src/main/java/com/MyManagement/entity/Student.java
@@ -0,0 +1,76 @@
+package com.MyManagement.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String rollNo;
+    private String email;
+    private String mobile;
+
+    public Student() {
+    }
+
+    public Student(int id, String firstName, String lastName, String rollNo, String email, String mobile) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.rollNo = rollNo;
+        this.email = email;
+        this.mobile = mobile;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(String rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+}

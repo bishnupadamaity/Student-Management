e895bdedbf2ba1230b9e78124a51baa2d453a7b6
Bishnupada Maity <bishnupdm.1@gmail.com>
2022-11-17 12:48:53 +0530

    adding update student feature

--- src/main/java/com/MyManagement/controller/StudentController.java
+++ src/main/java/com/MyManagement/controller/StudentController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -37,6 +38,30 @@ public class StudentController {
     @PostMapping("/students")
     public String saveStudent(@ModelAttribute("student") Student student){
         studentService.saveStudent(student);
+        return "redirect:/students";
+    }
+
+    @GetMapping("/students/edit/{id}")
+    public String editStudent(@PathVariable Long id, Model model){
+        model.addAttribute("student",studentService.getStudentById(id));
+        return "edit_student";
+    }
+    @PostMapping("/students/{id}")
+    public String updateStudent(
+            @PathVariable Long id,
+            @ModelAttribute("student") Student student,
+            Model model){
+        // get student from database by id
+        Student existingStudent = studentService.getStudentById(id);
+        existingStudent.setFirstName(student.getFirstName());
+        existingStudent.setLastName(student.getLastName());
+        existingStudent.setEmail(student.getEmail());
+        existingStudent.setRollNo(student.getRollNo());
+        existingStudent.setMobile(student.getMobile());
+        existingStudent.setId(student.getId());
+
+        // save updated student object
+        studentService.updateStudent(existingStudent);
         return "redirect:/students";
     }
 }
